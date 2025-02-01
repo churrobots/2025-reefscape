88e691022242e8df6f2de703d433f0da2f0877d4
Churrobots Software Team <55808130+epa-robotics-team@users.noreply.github.com>
2025-02-01 11:31:27 -0800

    Add AutoBuilder

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,12 @@ package frc.robot;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -185,11 +190,50 @@ public class RobotContainer {
   }
 
   Supplier<Command> bindCommandsForAutonomous() {
+
+    RobotConfig config;
+    try {
+      config = RobotConfig.fromGUISettings();
+      AutoBuilder.configure(
+          drivetrain::getPose, // Robot pose supplier
+          drivetrain::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+          drivetrain::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+          (speeds, feedforwards) -> drivetrain.setRobotRelativeSpeeds(speeds), // Method that will drive the robot given
+                                                                               // ROBOT RELATIVE ChassisSpeeds. Also
+                                                                               // optionally outputs individual module
+                                                                               // feedforwards
+          new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for
+                                          // holonomic drive trains
+              new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+              new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+          ),
+          config, // The robot configuration
+          () -> {
+            // Boolean supplier that controls when the path will be mirrored for the red
+            // alliance
+            // This will flip the path being followed to the red side of the field.
+            // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+            var alliance = DriverStation.getAlliance();
+            if (alliance.isPresent()) {
+              return alliance.get() == DriverStation.Alliance.Red;
+            }
+            return false;
+          },
+          drivetrain // Reference to this subsystem to set requirements
+      );
+    } catch (Exception e) {
+      // Handle exception as needed
+      e.printStackTrace();
+    }
+
     // TODO: make real commands for auto to use
-    NamedCommands.registerCommand("doNothing", new InstantCommand());
-    // TODO: use AutoBuilder.buildAutoChooser() instead
-    SendableChooser<Command> autoChooser = new SendableChooser<Command>();
-    SmartDashboard.putData(autoChooser);
+    NamedCommands.registerCommand("move1Beta", new InstantCommand());
+    NamedCommands.registerCommand("move2Sigma", new InstantCommand());
+    NamedCommands.registerCommand("move3Alpha", new InstantCommand());
+    NamedCommands.registerCommand("waitForTeammates", new InstantCommand());
+    SendableChooser<Command> autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto Chooser", autoChooser);
     return autoChooser::getSelected;
   }
 
