feda1b655fe91849c13e5e3e8d1970b558c73761
Churrobots Team <55808130+epa-robotics-team@users.noreply.github.com>
2025-02-03 21:35:56 -0800

    brought in all example code from YAGSL

--- src/main/java/frc/robot/subsystems/Drivetrain2.java
+++ src/main/java/frc/robot/subsystems/Drivetrain2.java
@@ -36,6 +36,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
 import frc.robot.Hardware;
+import frc.robot.subsystems.Vision2.Cameras;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -224,6 +225,21 @@ public class Drivetrain2 extends SubsystemBase {
    *
    * @return A {@link Command} which will run the alignment.
    */
+  public Command aimAtTarget(Cameras camera) {
+
+    return run(() -> {
+      Optional<PhotonPipelineResult> resultO = camera.getBestResult();
+      if (resultO.isPresent()) {
+        var result = resultO.get();
+        if (result.hasTargets()) {
+          drive(getTargetSpeeds(0,
+              0,
+              Rotation2d.fromDegrees(result.getBestTarget()
+                  .getYaw()))); // Not sure if this will work, more math may be required.
+        }
+      }
+    });
+  }
 
   /**
    * Get the path follower with events.
