41e590eedce83306d23b780759ea8f32f1f623c5
Churrobots Team <55808130+epa-robotics-team@users.noreply.github.com>
2025-02-04 19:09:49 -0800

    minor elevator cleanups

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -9,7 +9,6 @@ import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.churrolib.simulation.SimulationRegistry;
 import frc.robot.Hardware;
@@ -31,7 +30,6 @@ public class Elevator extends SubsystemBase {
     setDefaultCommand(stop());
     SimulationRegistry.registerHardware(m_elevatorMotorLeader);
     SimulationRegistry.registerHardware(m_elevatorMotorFollow);
-    // TODO: add PID configuration
 
     Slot0Configs slot0Configs = new Slot0Configs();
     slot0Configs.kP = Constants.kP;
@@ -39,7 +37,6 @@ public class Elevator extends SubsystemBase {
     slot0Configs.kD = Constants.kD;
 
     m_elevatorMotorLeader.getConfigurator().apply(slot0Configs);
-
   }
 
   public Command stop() {
@@ -49,51 +46,51 @@ public class Elevator extends SubsystemBase {
     });
   }
 
-  // Intake
+  // Move Elevator to Receiving position (this is the default position).
   public Command moveToRecieve() {
     return run(() -> {
       double desiredOutputInRotations = 4;
       double requiredInputRotations = Hardware.Elevator.gearboxReduction * desiredOutputInRotations;
       // create a position closed-loop request, voltage output, slot 0 configs
-      final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
+      final PositionVoltage m_request = new PositionVoltage(requiredInputRotations).withSlot(0);
 
-      m_elevatorMotorLeader.setControl(m_request.withPosition(requiredInputRotations));
+      m_elevatorMotorLeader.setControl(m_request);
     });
   }
 
-  // trough
+  // Move Elevator to L1 (trough).
   public Command move1Beta() {
     return run(() -> {
       double desiredOutputInRotations = 8;
       double requiredInputRotations = Hardware.Elevator.gearboxReduction * desiredOutputInRotations;
       // create a position closed-loop request, voltage output, slot 0 configs
-      final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
+      final PositionVoltage m_request = new PositionVoltage(requiredInputRotations).withSlot(0);
 
-      m_elevatorMotorLeader.setControl(m_request.withPosition(requiredInputRotations));
+      m_elevatorMotorLeader.setControl(m_request);
     });
   }
 
-  // L2
+  // Move Elevator to L2.
   public Command move2Sigma() {
     return run(() -> {
       double desiredOutputInRotations = 16;
       double requiredInputRotations = Hardware.Elevator.gearboxReduction * desiredOutputInRotations;
       // create a position closed-loop request, voltage output, slot 0 configs
-      final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
+      final PositionVoltage m_request = new PositionVoltage(requiredInputRotations).withSlot(0);
 
-      m_elevatorMotorLeader.setControl(m_request.withPosition(requiredInputRotations));
+      m_elevatorMotorLeader.setControl(m_request);
     });
   }
 
-  // L3
+  // Move Elevator to L3.
   public Command move3Alpha() {
     return run(() -> {
       double desiredOutputInRotations = 24;
       double requiredInputRotations = Hardware.Elevator.gearboxReduction * desiredOutputInRotations;
       // create a position closed-loop request, voltage output, slot 0 configs
-      final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
+      final PositionVoltage m_request = new PositionVoltage(requiredInputRotations).withSlot(0);
 
-      m_elevatorMotorLeader.setControl(m_request.withPosition(requiredInputRotations));
+      m_elevatorMotorLeader.setControl(m_request);
     });
   }
 
