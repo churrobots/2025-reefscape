53ae98d7fafce6acd0d06d6cf5981ef531921f24
Churrobots Software Team <55808130+epa-robotics-team@users.noreply.github.com>
2025-02-08 22:19:12 -0800

    fixed out-of-bounds error and used hardware constants

--- src/main/java/frc/robot/subsystems/UnnecessaryLEDS.java
+++ src/main/java/frc/robot/subsystems/UnnecessaryLEDS.java
@@ -4,29 +4,28 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.LEDPattern;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Hardware;
 
 public class UnnecessaryLEDS extends SubsystemBase {
-  /** Creates a new UnnecessaryLEDS. */
-  private int chaseIndex = 0;
 
-  public static final class Constants {
-    public static final int leftPixelCount = 144;
-    public static final int rightPixelCount = 144;
-    public static final int totalPixels = leftPixelCount + rightPixelCount;
-  }
+  final AddressableLED m_leds = new AddressableLED(Hardware.LEDLights.ledPWM);
+  final AddressableLEDBuffer m_pixels = new AddressableLEDBuffer(
+      Hardware.LEDLights.leftPixelCount + Hardware.LEDLights.rightPixelCount);
 
-  final AddressableLED m_hardwareLEDs = new AddressableLED(9);
-  final AddressableLEDBuffer m_theoreticalLEDs = new AddressableLEDBuffer(Constants.totalPixels);
+  AddressableLEDBufferView m_leftPixels = m_pixels
+      .createView(0, Hardware.LEDLights.leftPixelCount - 1);
+
+  AddressableLEDBufferView m_rightPixels = m_pixels
+      .createView(Hardware.LEDLights.leftPixelCount, Hardware.LEDLights.rightPixelCount - 1)
+      .reversed();
+
   final LEDPattern m_offPattern = LEDPattern.kOff;
   final LEDPattern m_blue = LEDPattern.solid(Color.kBlue);
   final LEDPattern m_green = LEDPattern.solid(Color.kGreen);
@@ -36,12 +35,9 @@ public class UnnecessaryLEDS extends SubsystemBase {
   final LEDPattern m_operatorControlPattern = LEDPattern.solid(Color.kRed);
   final LEDPattern m_driverControlPattern = LEDPattern.solid(Color.kWhiteSmoke);
 
-  AddressableLEDBufferView m_left = m_theoreticalLEDs.createView(0, Constants.leftPixelCount);
-  AddressableLEDBufferView m_right = m_theoreticalLEDs.createView(Constants.leftPixelCount, Constants.totalPixels);
-
   public UnnecessaryLEDS() {
-    m_hardwareLEDs.setLength(m_theoreticalLEDs.getLength());
-    m_hardwareLEDs.start();
+    m_leds.setLength(m_pixels.getLength());
+    m_leds.start();
     setDefaultCommand(disable());
   }
 
@@ -83,7 +79,7 @@ public class UnnecessaryLEDS extends SubsystemBase {
 
   public Command yuvrajalliseeisredwhenigoupsettyspaghetti() {
     return run(() -> {
-      applyPattern(m_operatorControlPattern);// m_operatorControlPattern
+      applyPattern(m_operatorControlPattern);
     });
   }
 
@@ -93,38 +89,9 @@ public class UnnecessaryLEDS extends SubsystemBase {
     });
   }
 
-  private void applyChasingEffect() {
-    // Clear LEDs before applying new positions
-    for (int i = 0; i < Constants.totalPixels; i++) {
-      m_theoreticalLEDs.setRGB(i, 0, 0, 0); // Turn off all LEDs
-    }
-
-    // Chasing light effect on left strip (forward direction)
-    int leftIndex = chaseIndex % Constants.leftPixelCount;
-    m_left.setRGB(leftIndex, 255, 0, 0);
-
-    // Chasing light effect on right strip (backward direction)
-    int rightIndex = Constants.rightPixelCount - (chaseIndex % Constants.rightPixelCount) - 1;
-    m_right.setRGB(rightIndex, 255, 0, 0);
-
-    // Increment chase index for next cycle
-    chaseIndex++;
-  }
-
   private void applyPattern(LEDPattern pattern) {
-    pattern.applyTo(m_theoreticalLEDs);
-    m_hardwareLEDs.setData(m_theoreticalLEDs);
-
-    for (int i = 0; i < Constants.rightPixelCount / 2; i++) {
-      int leftIndex = Constants.leftPixelCount + i;
-      int rightIndex = Constants.totalPixels - 1 - i;
-
-      // Swap colors to simulate reversing
-      Color temp = m_theoreticalLEDs.getLED(leftIndex);
-      m_theoreticalLEDs.setLED(leftIndex, m_theoreticalLEDs.getLED(rightIndex));
-      m_theoreticalLEDs.setLED(rightIndex, temp);
-    }
-
-    m_hardwareLEDs.setData(m_theoreticalLEDs);
+    pattern.applyTo(m_leftPixels);
+    pattern.applyTo(m_rightPixels);
+    m_leds.setData(m_pixels);
   }
 }
