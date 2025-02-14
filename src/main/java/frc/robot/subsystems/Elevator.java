ab9f16828eccbc0529f1fac720ff00b17a2015b3
Churrobots Team <55808130+epa-robotics-team@users.noreply.github.com>
2025-02-13 22:06:24 -0800

    minor cleanups in elevator
    updated documentation
    added kS, kV, kA
    moved numbers to Constants

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -26,9 +26,26 @@ public class Elevator extends SubsystemBase {
   class Constants {
     // PID numbers from:
     // https://v6.docs.ctr-electronics.com/en/2024/docs/api-reference/device-specific/talonfx/basic-pid-control.html
-    static final double kP = 0.8; // An error of 1 rotation results in 2.4 V output
-    static final double kI = 0.0; // no output for integrated error
-    static final double kD = 0.0; // A velocity of 1 rps results in 0.1 V output
+    static final double kS = 0.0; // static friction feed-forward component.
+    static final double kV = 0.12; // motor-specific, represents the motor's velocity per volt. The reciprocal is
+                                   // used as the feed-forward parameter in velocity-based control loops.
+    static final double kA = 0.0; //
+    static final double kP = 0.0; // proportional
+    static final double kI = 0.0; // integral
+    static final double kD = 0.0; // derivative
+
+    // Maximum velocity in rotations per second of the post-gearbox sprocket (NOT of
+    // the motor).
+    static final double kMaxVelocity = 0.5 * Hardware.Elevator.gearboxReduction;
+    static final double kMaxAcceleration = 1 * Hardware.Elevator.gearboxReduction;
+    static final double kMaxJerk = 10 * Hardware.Elevator.gearboxReduction;
+
+    // Desired Vertical Travel in meters for each position. Heights are specified
+    // relative to the base position. Total elevator travel ranges from 0 to ~35cm.
+    static final double kBaseHeight = 0.0; // initial height, also the height where we receive coral.
+    static final double kL1Height = 0.11; // height to score in L1 trough
+    static final double kL2Height = 0.22; // height to score in L2
+    static final double kL3Height = 0.33; // height to score in L3
   }
 
   public Elevator() {
@@ -36,26 +53,33 @@ public class Elevator extends SubsystemBase {
     HardwareRegistry.registerHardware(m_elevatorMotorLeader);
     HardwareRegistry.registerHardware(m_elevatorMotorFollow);
 
+    talonFXConfigs.Slot0.kD = Constants.kS;
+    talonFXConfigs.Slot0.kD = Constants.kV;
+    talonFXConfigs.Slot0.kD = Constants.kA;
     talonFXConfigs.Slot0.kP = Constants.kP;
     talonFXConfigs.Slot0.kI = Constants.kI;
     talonFXConfigs.Slot0.kD = Constants.kD;
 
     // https://v6.docs.ctr-electronics.com/en/2024/docs/api-reference/device-specific/talonfx/motion-magic.html
-    // NOTE: these rps (rotations per second) values are on the motor side, before
-    // the gearbox reduction. So the rotations of the sprocket will be fewer.
-    talonFXConfigs.MotionMagic.MotionMagicCruiseVelocity = 1; // Target cruise velocity of 1 rps
-    talonFXConfigs.MotionMagic.MotionMagicAcceleration = 2; // Target acceleration of 2 rps/s (0.5 seconds to reach max
-                                                            // target velocity of 1rps)
-    talonFXConfigs.MotionMagic.MotionMagicJerk = 20; // Target jerk of 20 rps/s/s (0.1 seconds)
+    // Motion Magic Voltage Control (Position):
+    // 1) accelerates to cruise velocity
+    // 2) maintains cruise velocity during cruise phase
+    // 3) decelerates as it approaches the target position
+    talonFXConfigs.MotionMagic.MotionMagicCruiseVelocity = Constants.kMaxVelocity;
+    talonFXConfigs.MotionMagic.MotionMagicAcceleration = Constants.kMaxAcceleration;
+    talonFXConfigs.MotionMagic.MotionMagicJerk = Constants.kMaxJerk;
 
     // Brake when motor is not driven.
     talonFXConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
+    // Apply all above configs to the Leader.
     m_elevatorMotorLeader.getConfigurator().apply(talonFXConfigs);
 
     // Initialize encoder position to zero.
     m_elevatorMotorLeader.setPosition(0);
 
+    // Set the follower to follow the leader. The follower is not inverted due to
+    // the geometry of the gearbox.
     m_elevatorMotorFollow.setControl(new Follower(m_elevatorMotorLeader.getDeviceID(), false));
 
   }
@@ -70,8 +94,7 @@ public class Elevator extends SubsystemBase {
   // Move Elevator to Receiving position (this is the default position).
   public Command moveToReceive() {
     return run(() -> {
-      double desiredVerticalTravelInMeters = 0;
-      double desiredOutputInRotations = desiredVerticalTravelInMeters / Hardware.Elevator.sprocketPitchDiameter;
+      double desiredOutputInRotations = Constants.kBaseHeight / Hardware.Elevator.sprocketPitchDiameter;
       double requiredInputRotations = Hardware.Elevator.gearboxReduction * desiredOutputInRotations;
 
       m_elevatorMotorLeader.setControl(m_request.withPosition(requiredInputRotations));
@@ -81,8 +104,7 @@ public class Elevator extends SubsystemBase {
   // Move Elevator to L1 (trough).
   public Command move1Beta() {
     return run(() -> {
-      double desiredVerticalTravelInMeters = 0.11;
-      double desiredOutputInRotations = desiredVerticalTravelInMeters / Hardware.Elevator.sprocketPitchDiameter;
+      double desiredOutputInRotations = Constants.kL1Height / Hardware.Elevator.sprocketPitchDiameter;
       double requiredInputRotations = Hardware.Elevator.gearboxReduction * desiredOutputInRotations;
 
       m_elevatorMotorLeader.setControl(m_request.withPosition(requiredInputRotations));
@@ -92,8 +114,7 @@ public class Elevator extends SubsystemBase {
   // Move Elevator to L2.
   public Command move2Sigma() {
     return run(() -> {
-      double desiredVerticalTravelInMeters = 0.22;
-      double desiredOutputInRotations = desiredVerticalTravelInMeters / Hardware.Elevator.sprocketPitchDiameter;
+      double desiredOutputInRotations = Constants.kL2Height / Hardware.Elevator.sprocketPitchDiameter;
       double requiredInputRotations = Hardware.Elevator.gearboxReduction * desiredOutputInRotations;
 
       m_elevatorMotorLeader.setControl(m_request.withPosition(requiredInputRotations));
@@ -103,8 +124,7 @@ public class Elevator extends SubsystemBase {
   // Move Elevator to L3.
   public Command move3Alpha() {
     return run(() -> {
-      double desiredVerticalTravelInMeters = 0.33;
-      double desiredOutputInRotations = desiredVerticalTravelInMeters / Hardware.Elevator.sprocketPitchDiameter;
+      double desiredOutputInRotations = Constants.kL3Height / Hardware.Elevator.sprocketPitchDiameter;
       double requiredInputRotations = Hardware.Elevator.gearboxReduction * desiredOutputInRotations;
 
       m_elevatorMotorLeader.setControl(m_request.withPosition(requiredInputRotations));
